5a7e2c9e309c1cc87444c088d006d5760227da53
vihitha <vihitha.batchu@ltts.com>
2020-11-20 11:30:24 +0530

    Initial commit

---
+++ src/main/java/mariocsee/android/clearskies/fragments/OnAddCityAnswer.java
@@ -0,0 +1,10 @@
+package mariocsee.android.clearskies.fragments;
+
+/**
+ * Created by mariocsee on 12/1/16.
+ */
+
+public interface OnAddCityAnswer {
+    public void onPositiveSelected();
+    public void onNegativeSelected();
+}
